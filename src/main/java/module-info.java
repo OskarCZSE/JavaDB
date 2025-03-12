7c15c76ac5a9d0314bbe10f2098b8fb8ebd5377b
OskarCZSE <oskarciebielski@proton.me>
2025-03-12 14:39:34 +0100

    Add files via upload

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,8 @@
-module com.example.javadb {
+module com.example.javadb2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
 
-
-    opens com.example.javadb to javafx.fxml;
-    exports com.example.javadb;
+    opens com.example.javadb2 to javafx.fxml;
+    exports com.example.javadb2;
 }
